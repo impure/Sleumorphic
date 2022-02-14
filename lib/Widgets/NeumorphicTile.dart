3fb22d2afb24ea6d7927b0184137c65f788cbb9d
Andrew Zuo <andrew@andrewzuo.com>
2022-02-14 15:56:40 -0500

    Disabled animation on the back tiles

--- lib/Widgets/NeumorphicTile.dart
+++ lib/Widgets/NeumorphicTile.dart
@@ -51,11 +51,14 @@ class NeumorphicTileState extends SyncState<Offset, NeumorphicTile> with SingleT
 
 	@override
 	void update(Offset? offset) {
-		Future<void>.delayed(Duration(milliseconds: (75 * ((widget.offset - offset!).distance - 1)).round())).then((_) {
-			_controller.forward(from: 0);
-		});
-
-		super.update(null);
+		if (widget.foreground) {
+			Future<void>.delayed(Duration(milliseconds: (75 * ((widget.offset - offset!).distance - 1)).round())).then((_) {
+				_controller.forward(from: 0);
+			});
+		} else {
+			displayNum = widget.num;
+			super.update(offset);
+		}
 	}
 
 	@override

68faf9e6655b9946b75c5f3b88d543b037e3516d
Andrew Zuo <andrew@andrewzuo.com>
2022-02-14 10:32:16 -0500

    Added another checkWin check

--- lib/Widgets/NeumorphicTile.dart
+++ lib/Widgets/NeumorphicTile.dart
@@ -126,6 +126,7 @@ class NeumorphicTileState extends SyncState<Offset, NeumorphicTile> with SingleT
 								puzzle.invertPieces();
 								boardStateGroup.notifyAll();
 								neumorphicTiles.notifyAll(widget.offset);
+								puzzle.checkWin(context);
 								return;
 							}
 							if (puzzle.solved) {

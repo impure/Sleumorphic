2e937a5fbe25f936db1da4a8944da70aafbf7c26
Andrew Zuo <andrew@andrewzuo.com>
2022-02-14 22:49:49 -0500

    Got rid of the icons in the web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -16,9 +16,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cbaf37547ee64342b550649d2317e37a",
-"/": "cbaf37547ee64342b550649d2317e37a",
-"main.dart.js": "2fd5641abbea29a9a8657d0c9f79319f",
+"index.html": "edbf8d3a32bb2d42ae83a841da281f4e",
+"/": "edbf8d3a32bb2d42ae83a841da281f4e",
+"main.dart.js": "0092dffddbc6e98bd0f353938b2892bd",
 "manifest.json": "e21842d5253c685cbfd2501e8f0d52a1",
 "version.json": "03f5ffc8da413c98287f7a86b4ddb635"
 };
